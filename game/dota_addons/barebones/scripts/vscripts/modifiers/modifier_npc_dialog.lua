49008c4cc4baab97d7bb8f9dec1b4b0f101b7dbe
EarthSalamander <jeizyy30@hotmail.fr>
2019-12-05 17:51:56 +0100

    Low attack priority on npc's

--- game/dota_addons/barebones/scripts/vscripts/modifiers/modifier_npc_dialog.lua
+++ game/dota_addons/barebones/scripts/vscripts/modifiers/modifier_npc_dialog.lua
@@ -29,6 +29,7 @@ function modifier_npc_dialog:CheckState()
 		[MODIFIER_STATE_DISARMED] = true,
 		[MODIFIER_STATE_NO_HEALTH_BAR] = true,
 		[MODIFIER_STATE_STUNNED] = true,
+		[MODIFIER_STATE_LOW_ATTACK_PRIORITY] = true,
 	}
 	
 	return state
